327260085d867810c80cf9f05f8875c38dfab84b
Davi Lima <dlima12435@gmail.com>
2024-11-09 14:49:21 -0300

    corrigindo palavras

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@ let data = new Date().getHours(22);
 if (data > 1 && data <= 12){
     document.getElementById("perfil").innerHTML = "Bom dia";
 }else if (data >= 13 && data <= 18){
-    document.getElementById("perfil").innerHTML = "Bom tarde";
+    document.getElementById("perfil").innerHTML = "Boa tarde";
 }else if (data >= 19 && data <= 24) {
     document.getElementById("perfil").innerHTML = "Boa noite";
 }else{
