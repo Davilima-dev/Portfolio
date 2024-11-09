e3fa3c672c30d67dd88647123c22743e21cf826e
Davi Lima <dlima12435@gmail.com>
2024-11-09 14:15:55 -0300

    atualizações

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+let data = new Date().getHours(22);
+
+if (data > 1 && data <= 12){
+    document.getElementById("perfil").innerHTML = "Bom dia";
+}else if (data >= 13 && data <= 18){
+    document.getElementById("perfil").innerHTML = "Bom tarde";
+}else if (data >= 19 && data <= 24) {
+    document.getElementById("perfil").innerHTML = "Boa noite";
+}else{
+    document.getElementById("perfil").innerHTML = "initial";
+}
