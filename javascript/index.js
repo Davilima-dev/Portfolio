78ede7f9a89fdd6198a5c9f6099c35e006d9f9c5
Davi Lima <dlima12435@gmail.com>
2024-12-13 19:08:52 -0300

    .

--- javascript/index.js
+++ javascript/index.js
@@ -2,10 +2,17 @@ let data = new Date().getHours();
 
 if (data > 1 && data <= 12){
     document.getElementById("perfil").innerHTML = "Bom dia";
-}else if (data >= 13 && data <= 18){
+}else if (data >= 13 && data <= 17){
     document.getElementById("perfil").innerHTML = "Boa tarde";
-}else if (data >= 19 && data <= 24) {
+}else if (data >= 18 && data <= 24) {
     document.getElementById("perfil").innerHTML = "Boa noite";
 }else{
     document.getElementById("perfil").innerHTML = "initial";
 }
+
+let contato = document.getElementById("ctt");
+contato.style.backgroundColor = "#ccc8c9";
+contato.style.color = "#3c84a4";
+contato.style.border = "none";
+contato.style.borderRadius = "5px";
+
